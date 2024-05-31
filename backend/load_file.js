102c8a90365e28e8f40fbc6aa4626b62ff5a6b78
Sahil <sahilrohilla779@gmail.com>
2024-06-01 04:59:29 +0530

    Update load_file.js

--- backend/load_file.js
+++ backend/load_file.js
@@ -2,7 +2,7 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const getMedicinesFromJSON = async () => {
-  const filePath = path.join(__dirname, 'data.json'); 
+  const filePath = path.join(__dirname, 'data2.json'); 
   const data = await fs.readFile(filePath, 'utf-8');
   return JSON.parse(data);
 };
