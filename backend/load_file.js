73e15aa17f81bfd1458f34f9194b025714eded7e
Sahil <sahilrohilla779@gmail.com>
2024-06-01 03:32:33 +0530

    Add files via upload

---
+++ backend/load_file.js
@@ -0,0 +1,9 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const getMedicinesFromJSON = async () => {
+  const filePath = path.join(__dirname, 'data.json'); 
+  const data = await fs.readFile(filePath, 'utf-8');
+  return JSON.parse(data);
+};
+
